6c13aec866fa6f67263b0edaeba48a71d3b1b202
LeHoangAnh21 <84314151+LeHoangAnh21@users.noreply.github.com>
2021-08-20 09:25:56 +0700

    first commit

---
+++ assets/app.js
@@ -0,0 +1,61 @@
+// app = {
+// 	currentIndex: 0,
+// 	isPlaying: false,
+// 	isRandom: false,
+// 	isRepeat: false,
+// 	songs: [
+// 		{
+// 			name: 'Xin Dung Lang Im',
+// 			singer: 'Soobin Hoang Son',
+// 			path: './assets/mp3/bai1.mp3',
+// 			image: './assets/photo/anh1.png'
+// 		},
+// 		{
+// 			name: 'Khong Muon Yeu Lai Cang Say Dam',
+// 			singer: 'Mr.Siro',
+// 			path: './assets/mp3/bai2.mp3',
+// 			image: './assets/photo/anh2.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Beautiful in white',
+// 			singer: 'Shane Filan',
+// 			path: './assets/mp3/bai3.mp3',
+// 			image: './assets/photo/anh3.jpg'
+// 		},
+// 		{
+// 			name: 'Cheating on you',
+// 			singer: 'Charlie Puth',
+// 			path: './assets/mp3/bai4.mp3',
+// 			image: './assets/photo/anh4.jpg'
+// 		}
+// 	],
